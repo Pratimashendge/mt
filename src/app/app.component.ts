623b983c77ca4b0eff7e444e1de2c1eb308e36f2
Pratimashendge <shendgepratima05@gmail.com>
2024-12-29 16:26:55 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Ipost } from './models/posts';
+import { posts } from './constant/posts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mt';
+  postsArr:Array<Ipost>=posts;
 }
